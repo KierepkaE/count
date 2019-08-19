09ca5eb7e6a19da31f2bd7807915dc3dc366eb5c
kierepkae <kierepkaedyta@gmail.com>
2019-08-19 13:55:06 +0100

    displaying 0 as default

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const button = document.getElementById("increment");
 const display = document.getElementById("display");
 let counter = 0;
-
+display.textContent = counter;
 function add() {
   counter++;
   display.textContent = counter;
